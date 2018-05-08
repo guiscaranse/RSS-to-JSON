ac79e8440c00a57f6e47e3bcddcb9883e8aa38e8
Guilherme Scaranse <guiscaranse@gmail.com>
2018-05-08 16:32:09 -0300

    Fixed error handling when rss feed is empty

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 var express = require('express');
 var app = express();
-var Feed = require('rss-to-json');
+const Feed = require('feed-to-json-promise')
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
-app.get('/', function(req, res, next) {
-  if (req.query.feedURL) {
-    Feed.load(req.query.feedURL, function(err, rss){
-      if (err) {
-        res.send({ 'error': 'An error has occurred' });
-      } else {
-        res.send(rss);
-      }
-    });
-  } else {
-    res.status(400).send({ 'error': 'feedURL is required' });
-  }
+app.get('/', function (req, res, next) {
+    const feed = new Feed();
+    if (req.query.feedURL) {
+        feed.load(req.query.feedURL, {timeout: 500}).then(feed => {
+            res.send(feed);
+        }).catch(error => {
+            res.send({"error": 'true', "items": []});
+        })
+    } else {
+        res.status(400).send({'error': 'feedURL is required'});
+    }
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/hi', function (req, res, next) {
+    res.status(200).send({'ping': 'pong'});
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
